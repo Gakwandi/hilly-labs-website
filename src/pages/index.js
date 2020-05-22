3bf25f8e15f6d0dcd9bfbd506574b2b2c4bba854
Gakwandi <gakwandijoshua@gmail.com>
2020-05-22 18:33:52 +0200

    Added the first interface

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
-import React from "react"
+import React from "react";
+import "../css/style.css";
+import logo from "../images/bg/Logo1.png";
 
-export default () => <div>Hello world!</div>
+export default  function index() {
+    return (
+        <div class="container">
+            <div class="content">
+                {/* <h1>Our website is almost ready</h1> */}
+                <img src={logo} alt="Hilly Labs"/>
+                <h1>Coming <span>Soon</span></h1>
+                <br/>
+                <p>Our website is currently undergoing scheduled maintenance.</p>
+                <p>We should be back shortly. Thank you for your patience</p>
+            </div>
+            
+        </div>
+    )
+}
